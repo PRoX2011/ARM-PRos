cbe45775758e786ff77e99c966657efb59026782
peoplemiau1 <eznaevsamil70@gmail.com>
2026-03-26 19:37:36 +0300

    Refactor atoi and itoa functions for clarity and safety

--- src/lib/stdlib.c
+++ src/lib/stdlib.c
@@ -1,78 +1,70 @@
 #include <stdlib.h>
+#include <limits.h>
 
 int atoi(const char *str) {
+    if (!str) return 0;
+
     int res = 0;
     int sign = 1;
-    int i = 0;
 
-    while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n') {
-        i++;
+    while (*str == ' ' || *str == '\t' || *str == '\n' || 
+           *str == '\r' || *str == '\v' || *str == '\f') {
+        str++;
     }
 
-    if (str[i] == '-') {
+    if (*str == '-') {
         sign = -1;
-        i++;
-    } else if (str[i] == '+') {
-        i++;
+        str++;
+    } else if (*str == '+') {
+        str++;
     }
 
-    for (; str[i] != '\0'; ++i) {
-        if (str[i] < '0' || str[i] > '9') {
-            break;
+    while (*str >= '0' && *str <= '9') {
+        int digit = *str - '0';
+
+        if (res > (INT_MAX - digit) / 10) {
+            return sign == 1 ? INT_MAX : INT_MIN;
         }
-        res = res * 10 + (str[i] - '0');
+
+        res = res * 10 + digit;
+        str++;
     }
 
     return sign * res;
 }
 
 char *itoa(int value, char *str, int base) {
-    if (base < 2 || base > 36) {
-        str[0] = '\0';
+    if (!str || base < 2 || base > 36) {
+        if (str) *str = '\0';
         return str;
     }
 
-    int i = 0;
-    int is_negative = 0;
-    unsigned int n;
+    char *ptr = str;
+    unsigned int n = (unsigned int)value;
 
     if (value < 0 && base == 10) {
-        is_negative = 1;
-        n = (unsigned int)(-(value + 1)) + 1u;
-    } else {
-        n = (unsigned int)value;
+        n = -n;
     }
 
-    if (n == 0) {
-        str[i++] = '0';
-        str[i] = '\0';
-        return str;
+    const char digits[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+    do {
+        *ptr++ = digits[n % base];
+        n /= base;
+    } while (n);
+
+    if (value < 0 && base == 10) {
+        *ptr++ = '-';
     }
 
-    while (n != 0) {
-        unsigned int rem = n % (unsigned int)base;
-        if (rem > 9) {
-            str[i++] = (char)(rem - 10 + 'a');
-        } else {
-            str[i++] = (char)(rem + '0');
-        }
-        n = n / (unsigned int)base;
-    }
+    *ptr = '\0';
 
-    if (is_negative) {
-        str[i++] = '-';
-    }
-
-    str[i] = '\0';
-
-    int start = 0;
-    int end = i - 1;
+    char *start = str;
+    char *end = ptr - 1;
     while (start < end) {
-        char temp = str[start];
-        str[start] = str[end];
-        str[end] = temp;
-        start++;
-        end--;
+        char temp = *start;
+        *start++ = *end;
+        *end-- = temp;
     }
 
     return str;
